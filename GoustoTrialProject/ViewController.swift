28c04d941d47bac465f5d7f52454efb527fb64e5
Mantas Laurinavicius <mantas@macbook-pro.22decembre.eu>
2016-06-10 14:45:30 +0200

    Initial Commit

---
+++ GoustoTrialProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoustoTrialProject
+//
+//  Created by Mantas Laurinavicius on 10/06/16.
+//  Copyright © 2016 Mashkius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
